2b60136f0310cfe156b0bd44dd1f5463151e5602
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-11-28 15:41:34 +0900

    google auth working

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,20 +1,56 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
+import * as Google from "expo-google-app-auth";
+import { GoogleAuthProvider, onAuthStateChanged, signInWithCredential, signOut } from '@firebase/auth';
+import { auth } from '../firebase';
+import {REACT_APP_API_KEY} from "@env";
+
 
 const AuthContext = createContext({
 
 });
 
+const config = {
+  iosClientId: REACT_APP_API_KEY,
+  scopes: ["profile", "email"],
+  permissions: ["public_profile", "email", "gender", "location"]
+}
+
 export const AuthProvider = ( { children }) => {
+  const [ error, setError ] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loadingInitial, setLoadingInitial] = useState(true);
+
+  useEffect(() => onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+
+      }
+      setLoadingInitial(false)
+    }), [])
+  const signInWithGoogle = async () => {
+    await Google.logInAsync(config).then(async (logInResult) => {
+      if(logInResult.type === 'success') {
+         const { idToken, accessToken } = logInResult;
+         const credential = GoogleAuthProvider.credential(idToken, accessToken);
+         await signInWithCredential(auth, credential);
+      }
+      return Promise.reject();
+      
+    }).catch(error => setError(error));
+  };
   return (
     <AuthContext.Provider value={{
-      user: null
+      user,
+      signInWithGoogle,
     }}>
-      {children}
+      {!loadingInitial && children}
     </AuthContext.Provider>
   )
 };
 
 export default function useAuth() {
-  return useContent(AuthContext);
+  return useContext(AuthContext);
 }
