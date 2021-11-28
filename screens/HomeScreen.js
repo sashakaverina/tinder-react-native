55ab429d2bb1183e10318c7752be80bb1cedd4d1
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-11-28 17:08:48 +0900

    set navbar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,53 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react'
-import { View, Text, Button } from 'react-native';
+import { Image, View, Text, Button, SafeAreaView, TouchableOpacity } from 'react-native';
 import useAuth from '../hooks/useAuth';
+import { useLayoutEffect } from 'react';
+import tw from 'tailwind-rn';
+import { AntDesign, Entypo, Ionicons} from "@expo/vector-icons";
 
 
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({headerShown: false,
+  });
+ }, []);
+
   return (
-    <View>
-      <Text>Heeee</Text>
+    <SafeAreaView>
+      <View style={tw('flex-row items-center justify-between px-5')}>
+        <TouchableOpacity onPress={logout}>
+          <Image style={tw('h-10 w-10 rounded-full')} source={{ uri: user.photoURL }}
+          />
+
+        </TouchableOpacity>
+
+     
+
+      <TouchableOpacity>
+        <Image
+          source={require('../tinder-logo.png')}
+          style={tw('h-14 w-14')}
+        
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+        <Ionicons name='chatbubbles-sharp' size={30} color="#FF5864"/>
+      </TouchableOpacity>
+      </View>
+      {/* <Text>Heeee</Text>
       <Button title="Go to chat screen" onPress={() => 
       navigation.navigate("Chat")
       }
       />
 
-      <Button title="log out" onPress={logout}/>
-    </View>
+      <Button title="log out" onPress={logout}/> */}
+    </SafeAreaView>
   )
 }
 
