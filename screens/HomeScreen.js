2548d5951517bbb686225647c01aa13dff1638f6
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-11-28 18:35:48 +0900

    match/nope

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react'
-import { Image, View, Text, Button, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, View, Text, Button, SafeAreaView, TouchableOpacity } from 'react-native';
 import useAuth from '../hooks/useAuth';
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 import tw from 'tailwind-rn';
 import { AntDesign, Entypo, Ionicons} from "@expo/vector-icons";
 import Swiper from "react-native-deck-swiper";
@@ -38,6 +38,7 @@ const DATA = [
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { user, logout } = useAuth();
+  const swipeRef = useRef();
 
   useLayoutEffect(() => {
     navigation.setOptions({headerShown: false,
@@ -68,23 +69,85 @@ const HomeScreen = () => {
       </TouchableOpacity>
       </View>
     <View style={tw('flex-1 -mt-6')}>
-    <Swiper 
+    <Swiper
+    ref={swipeRef} 
     containerStyle={{ backgroundColor: "transparent"}}
     cards={DATA}
+    stackSize={5}
+    cardIndex={0}
+    verticalSwipe={false}
+    onSwipedLeft={() => {
+
+    }}
+    overlayLabels={{
+      left: {
+        title: "NOPE",
+        style: {
+          label: {
+            textAlign: "right",
+            color: "red",
+          },
+        },
+      },
+      right: {
+        title: "MATCH",
+        style: {
+          label: {
+            
+            color: "#4DED30",
+          },
+        },
+      }
+    }}
+    animateCardOpacity
       renderCard={card => (
-        <View key={card.id} style={tw('relative bg-white h-3/4 rounded-xl')}>
-          <Image
-          source={{ uri: card.photoUrl}}
-          style={tw('absolute top-0 h-full w-full rounded-xl')}
-          />
+        <View 
+        key={card.id} style={tw('relative bg-white h-3/4 rounded-xl')}>
+              <Image
+              source={{ uri: card.photoUrl}}
+              style={tw('absolute top-0 h-full w-full rounded-xl')}
+              />
+        <View style={[tw('absolute bottom-0 bg-white w-full justify-between px-6 py-2 items-center flex-row h-20 rounded-b-xl'), styles.cardShadow]}>
+          <View >
+            <Text style={tw('text-xl font-bold')}>{card.firstName} {card.lastName}</Text>
+            <Text>{card.occupation}</Text>
+          </View>
+          <Text style={tw('text-2xl font-bold')}>{card.age}</Text>
+          </View>  
         </View>
-      )}
-    />
-    </View>
+        )}
+        />
+        </View>
+
+        <View style={tw('flex flex-row justify-evenly')}>
+          <TouchableOpacity 
+          onPress={() => swipeRef.current.swipeLeft()}
+          style={tw('items-center justify-center rounded-full w-16 h-16 bg-red-200')}>
+            <Entypo name="cross" size={24} color="red"/>
+          </TouchableOpacity>
+          <TouchableOpacity 
+          onPress={() => swipeRef.current.swipeRight()}
+          style={tw('items-center justify-center rounded-full w-16 h-16 bg-green-200')}>
+            <AntDesign name="heart" size={24} color="green"/>
+          </TouchableOpacity>
+        </View>
 
 
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  }
+})
 
 export default HomeScreen
